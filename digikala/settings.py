ab345e30a797674d44371b91848fb4db8d41e734
Mahdi-M-Back <mmahdibusiness2025@gmail.com>
2025-07-21 10:10:20 +0330

    Update settings.py

--- digikala/settings.py
+++ digikala/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-lsmba6h4$c5edpg2r5v5a9%4*ps5zfu+7o*=_n1i8lqcgmjmxe"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://digikala-z7bs.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 STATICFILES_USER = ["static/"]
 MEDIA_URL = "media/"
