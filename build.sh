38fe802fd59a868562b8c89c374a56709f33d77a
Mahdi-M-Back <mmahdibusiness2025@gmail.com>
2025-07-21 10:49:07 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-set -o errexit
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py createsuperuser --noinput
+#!/bin/bash
+
 python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
